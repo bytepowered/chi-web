7f54b5f8e0fceb560bf068a5b79c18545e176f67
chenxxpro <chenxxpro@hotmail.com>
2023-11-30 15:44:35 +0800

    feat: Add send json

--- go.mod
+++ go.mod
@@ -3,3 +3,12 @@ module github.com/bytepowered/chiweb
 go 1.21
 
 require github.com/go-chi/chi/v5 v5.0.10
+
+require (
+	github.com/bytedance/sonic v1.10.2 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+)
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package chiweb
 import (
 	"context"
 	"errors"
+	"github.com/bytedance/sonic"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log"
@@ -86,6 +87,31 @@ func SendTEXT(w http.ResponseWriter, statusCode int, data []byte) (int, error) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(statusCode)
 	return w.Write(data)
+}
+
+func SendJSONObjectOK(w http.ResponseWriter, data any) {
+	SendJSONObject(w, http.StatusOK, data)
+}
+
+func SendJSONObject(w http.ResponseWriter, statusCode int, data any) {
+	bytes, err := sonic.ConfigFastest.Marshal(map[string]any{
+		"status": "success",
+		"data":   data,
+	})
+	if err != nil {
+		slog.Error("app: failed to marshal data: %v", err)
+		SendERROR(w, http.StatusInternalServerError, "data serialized error")
+	} else {
+		SendJSON(w, statusCode, bytes)
+	}
+}
+
+func SendERROR(w http.ResponseWriter, statusCode int, msg string) {
+	bytes, _ := sonic.ConfigFastest.Marshal(map[string]any{
+		"status": "error",
+		"msg":    msg,
+	})
+	SendJSON(w, statusCode, bytes)
 }
 
 func newDefaultRootRouter() chi.Router {
